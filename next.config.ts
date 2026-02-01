c9f92c7b754129363a9e9b08067d803de6cc4c5a
nguyenhaidangb5-cmyk <nguyenhaidangb5@gmail.com>
2026-02-01 14:26:02 +0700

    fix anh

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // Quan trọng: Tắt tối ưu hóa để trình duyệt tự tải ảnh (tránh bị server chặn)
-    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'admin.anuongcangiuoc.org',
         pathname: '/**',
       },
+      // --- THÊM ĐOẠN NÀY ---
       {
-        protocol: 'http',
+        protocol: 'http',  // Cho phép cả HTTP thường
         hostname: 'admin.anuongcangiuoc.org',
         pathname: '/**',
       },
+      // --------------------
       {
         protocol: 'https',
         hostname: 'anuongcangiuoc.org',
@@ -31,7 +31,6 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
   },
-  // Bỏ qua lỗi check type để ưu tiên build thành công
   typescript: {
     ignoreBuildErrors: true,
   },
